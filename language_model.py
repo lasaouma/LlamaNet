113193637e90c5128dde027e5e59cc74e76ccd14
axnedergaard <axnedergaard@gmail.com>
2018-04-14 19:30:24 +0200

    Update language model according to project document (not sure if working). Add todo list of tasks.

--- language_model.py
+++ language_model.py
@@ -7,10 +7,10 @@ data = load_preprocessed_data()
 vocab,inv_vocab = load_vocab()
 
 #hyperparameters
-hidden_size = 32
-batch_size = 32
-learning_rate = 0.0001
-embedding_size = 16
+hidden_size = 512
+batch_size = 64
+learning_rate = 0.001
+embedding_size = 100
 n_train_steps = 100000
 
 vocab_size = len(vocab)
@@ -18,7 +18,8 @@ time_steps = len(data[0])
 input_n = time_steps
 output_n = vocab_size
 
-class Batcher: #TODO
+#batcher for getting shuffled training samples
+class Batcher:
     def __init__(self, data):
         self.data = np.array(data)
         self.data_size = len(data)
@@ -37,52 +38,71 @@ class Batcher: #TODO
         self.current_index += batch_size
         return batch_data
 
-#construct neural network #TODO seemingly doesn't work
+#construct neural network 
 tf.reset_default_graph()
-        
+
+
+#placeholders for inputs and targets
 with tf.variable_scope("inputs"):
     inputs = tf.placeholder(dtype=tf.int32, shape=[batch_size, input_n], name="inputs") 
     targets = tf.placeholder(dtype=tf.int32, shape=[batch_size, input_n], name="targets")
-    targets_one_hot = tf.one_hot(targets, vocab_size)
 
+#get embedding vectors
 with tf.variable_scope("embeddings"):
-    embedding_weights = tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0)
+    #embedding weight matrix
+    embedding_weights = tf.get_variable("embedding_weights", shape=[vocab_size, embedding_size], initializer=tf.contrib.layers.xavier_initializer())
+    #transform inputs to one hot vectors
     inputs_one_hot = tf.one_hot(inputs, vocab_size)
+    #tensorflow function for embedding
     embedded_inputs = tf.nn.embedding_lookup(embedding_weights, inputs)
-    #mbedded_inputs = inputs_one_hot
 
 with tf.variable_scope("rnn"):
-
+    #define lstm cell
     rnn = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size)
+    #state of lstm cell
     initial_state = state = tf.nn.rnn_cell.LSTMStateTuple(tf.zeros([batch_size, hidden_size]), tf.zeros([batch_size, hidden_size])) 
-    output_weights = tf.random_uniform([hidden_size, embedding_size], -1.0, 1.0)
-    output_bias = tf.random_uniform([embedding_size], -1.0, 1.0)
+    #shared weights and bias for output layer ("softmax")
+    output_weights = tf.get_variable("output_weights", shape=[hidden_size, embedding_size], initializer=tf.contrib.layers.xavier_initializer())
+    output_bias = tf.get_variable("output_bias", shape=[embedding_size], initializer=tf.contrib.layers.xavier_initializer())
 
+    
+    #fancy way of interating over time_steps which is middle dimension of embedded_inputs (batch_size x time_steps x embedding_size)
     output_list = []
-    prediction_list = []
+    prediction_list = [] #not required for training
     embedded_inputs_unstacked = tf.unstack(embedded_inputs, axis=1)
     for embedded_input in embedded_inputs_unstacked:
+        #calculate next state of rnn cell
         _, state = rnn(embedded_input, state)
+        #calculate output layer ("softmax")
         output = tf.matmul(state.h, output_weights) + output_bias
+        #"de-embed" softmax ???
         output_decoded = tf.matmul(output, tf.transpose(embedding_weights))
         output_list += [output_decoded]
-
-        prediction_list += [tf.argmax(output,axis=1)]
+        prediction_list += [tf.argmax(output,axis=1)] #not required for training
     outputs = tf.stack(output_list, axis=1)
-    predictions = tf.stack(prediction_list, axis=1)
+    predictions = tf.stack(prediction_list, axis=1) #not required for training
 
 with tf.variable_scope("optimizer"):
-    loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=outputs[:,:-1,:], labels=targets_one_hot[:,1:,:]) #correct?
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+    #calculate loss
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=outputs[:,:-1,:], labels=targets[:,1:])
+    #define optimizer
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    #clip gradients
+    var = tf.trainable_variables()
+    grad,_ = tf.clip_by_global_norm(tf.gradients(loss, var), 5)
+    #define training operation
+    train_op = optimizer.apply_gradients(zip(grad,var))
 
+#initialise graph
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
+#perform training step and return loss
 def train(x, y):
-    l,_ = sess.run([loss, optimizer], feed_dict={inputs: x, targets: y})
+    l,_ = sess.run([loss, train_op], feed_dict={inputs: x, targets: y})
     return l
 
-def predict(x):
+def predict(x): #not required for training
     return sess.run(predictions, feed_dict={inputs: x})
 
 
@@ -95,7 +115,7 @@ batcher = Batcher(data)
 for i in range(n_train_steps):
     x = y = batcher.get_batch(batch_size)
     l = train(x, y)
-    if i % 1000 == 0:
+    if i % 1 == 0:
         print("{:.1f}% loss={}".format(100*i/n_train_steps, l[0][0]))
 
 #test
