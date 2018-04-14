cdbf7b87e549d8874df0b4e47e65d23d8fff66ca
axnedergaard <axnedergaard@gmail.com>
2018-04-14 12:31:15 +0200

    Update language model draft. Still doesn't seem to learn...

--- language_model.py
+++ language_model.py
@@ -1,46 +1,90 @@
 import tensorflow as tf
 import numpy as np
 
-class NeuralLanguageModel:
-    def __init__(self, sentence_len=30, vocab_size=100):
-        self.hidden_size = 32
-        self.batch_size = 1
-        self.learning_rate = 0.001
-        self.input_n = sentence_len
-        self.output_n = vocab_size
-        #TODO missing vocab information?
+vocab_size = 6
+time_steps = 2
+hidden_size = 2
+batch_size = 32
+learning_rate = 0.001
+input_n = time_steps
+output_n = vocab_size
+embedding_size = 3
+n_train_steps = 50000
 
-        #construct neural network
-        tf.reset_default_graph()
+class Batcher:
+    def __init__(self, data):
+        self.data = data
+    def get_batch(self):
+        #retrieve random x,y
+        return np.reshape([self.data]*batch_size, [batch_size, input_n]), np.reshape([self.data]*batch_size, [batch_size, input_n])
+
+#construct neural network
+tf.reset_default_graph()
         
-        rnn = tf.contrib.rnn.BasicRNNCell(num_units=self.hidden_size)
+with tf.variable_scope("inputs"):
+    inputs = tf.placeholder(dtype=tf.int32, shape=[batch_size, input_n], name="inputs") 
+    targets = tf.placeholder(dtype=tf.int32, shape=[batch_size, input_n], name="targets")
+    targets_one_hot = tf.one_hot(targets, vocab_size)
 
-        self.inputs = tf.placeholder(dtype=tf.float32, shape=[self.batch_size, self.input_n], name="inputs") #TODO float32->int32?
-        initial_state = state = tf.zeros([self.batch_size, self.hidden_size])
+initial_state = state = tf.nn.rnn_cell.LSTMStateTuple(tf.zeros([batch_size, hidden_size]), tf.zeros([batch_size, hidden_size])) 
 
-        #input_embeddings tf.nn.embedding_lookup(embedding_matrix, inputs_ids) TODO
+with tf.variable_scope("embeddings"):
+    embedding_weights = tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0)
+    inputs_one_hot = tf.one_hot(inputs, vocab_size)
+    embedded_inputs = tf.nn.embedding_lookup(embedding_weights, inputs)
+    #mbedded_inputs = inputs_one_hot
 
-        for i in range(self.input_n):
-            output, state = rnn(tf.reshape(self.inputs[:,i], [self.batch_size,1]), state)
-        
-        self.final_state = state 
-        self.softmax = tf.nn.softmax(self.final_state)
-        self.prediction = tf.argmax(self.softmax, axis=1)
-        
-        #TODO de-embed prediction
-        
-        #TODO target, loss, optimizer
+output_list = []
+prediction_list = []
 
-        self.sess = tf.Session()
-        self.sess.run(tf.global_variables_initializer())
+with tf.variable_scope("rnn"):
+    rnn = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size)
+    output_weights = tf.random_uniform([hidden_size, vocab_size], -1.0, 1.0)
 
-    def train(self, x, y):
-        #loss,_ = self.sess.run([loss, optimiser], feed_dict={self.inputs: x, self.targets: y}) TODO
-        return loss
+    embedded_inputs_unstacked = tf.unstack(embedded_inputs, axis=1)
+    for embedded_input in embedded_inputs_unstacked:
+        _, state = rnn(embedded_input, state)
+        output = tf.matmul(state.h, output_weights)
+        output_list += [output]
+        prediction_list += [tf.argmax(output,axis=1)]
 
-    def predict(self, x):
-        return self.sess.run(self.prediction, feed_dict={self.inputs: x})[0] #TODO prediction for every timestep, not just final?
+    outputs = tf.stack(output_list, axis=1)
+    predictions = tf.stack(prediction_list, axis=1)
 
-data = [i for i in range(30)]
-nn = NeuralLanguageModel()
-print(nn.predict(np.reshape(data, [1,30])))
+with tf.variable_scope("optimizer"):
+    loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=outputs[:,:-1,:], labels=targets_one_hot[:,:1,:]) #correct?
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+def train(x, y):
+    l,_ = sess.run([loss, optimizer], feed_dict={inputs: x, targets: y})
+    return l
+
+def predict(x):
+    return sess.run(predictions, feed_dict={inputs: x})
+
+#driver
+#generate dummy data and vocab
+#data = [np.random.randint(0,vocab_size) for i in range(input_n)]
+data = [i for i in range(input_n)]
+vocab = dict()
+for i in range(vocab_size):
+    vocab[i] = str(i)
+
+batcher = Batcher(data)
+
+for i in range(n_train_steps):
+    x,y = batcher.get_batch()
+    l = train(x, y)
+    if i % 1000 == 0:
+        print("{:.1f}% loss={}".format(100*i/n_train_steps, l[0][0]))
+
+writer = tf.summary.FileWriter("./log", sess.graph)
+writer.close()
+
+predicted = predict(np.reshape([data]*batch_size, [batch_size, input_n]))
+
+for i in range(input_n-1): 
+    print(predicted[:,i], data[i+1])
