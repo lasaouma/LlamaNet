2a9fc4d6421e4be9d0f7af72b541785dd7d13e28
axnedergaard <axnedergaard@gmail.com>
2018-04-17 13:17:33 +0200

    Tidy code for language model significantly, including removal of deembed code.

--- language_model.py
+++ language_model.py
@@ -6,7 +6,7 @@ from batcher import *
 import datetime
 import os
 
-deembed = False
+#LOAD DATA AND SET PARAMETERS
 use_word2vec = False #set to true for Experiment B
 down_project = False #set to true for Experiment C
 
@@ -34,16 +34,12 @@ tf.reset_default_graph()
 #placeholders for inputs and targets
 with tf.variable_scope("inputs"):
     inputs = tf.placeholder(dtype=tf.int32, shape=[batch_size, input_n], name="inputs") 
+with tf.variable_scope("targets"):
     targets = tf.placeholder(dtype=tf.int32, shape=[batch_size, input_n], name="targets")
 
-#get embedding vectors
 with tf.variable_scope("embeddings"):
-    #embedding weight matrix
-    if use_word2vec: #non-trainable if using pretrained weights
-        embedding_weights = tf.get_variable("embedding_weights", shape=[vocab_size, embedding_size], initializer=tf.contrib.layers.xavier_initializer(), trainable=False)
-    else:
-        embedding_weights = tf.get_variable("embedding_weights", shape=[vocab_size, embedding_size], initializer=tf.contrib.layers.xavier_initializer())
-    #tensorflow function for embedding
+    embedding_weights = tf.get_variable("embedding_weights", shape=[vocab_size, embedding_size], initializer=tf.contrib.layers.xavier_initializer())
+    #tensorflow function for embedding (under hood: convert input to one hot representation and multiply with embedding weight matrix)
     embedded_inputs = tf.nn.embedding_lookup(embedding_weights, inputs)
 
 with tf.variable_scope("rnn"):
@@ -54,43 +50,41 @@ with tf.variable_scope("rnn"):
     else:
         rnn = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size, name="lstm_cell")
         initial_state = state = tf.nn.rnn_cell.LSTMStateTuple(tf.zeros([batch_size, hidden_size]), tf.zeros([batch_size, hidden_size])) 
+    
     #shared weights and bias for output layer
-    if deembed:
-        output_weights = tf.get_variable("output_weights", shape=[hidden_size, embedding_size], initializer=tf.contrib.layers.xavier_initializer())
-        output_bias = tf.get_variable("output_bias", shape=[embedding_size], initializer=tf.contrib.layers.xavier_initializer())
-    else:
-        output_weights = tf.get_variable("output_weights", shape=[hidden_size, vocab_size], initializer=tf.contrib.layers.xavier_initializer())
-        output_bias = tf.get_variable("output_bias", shape=[vocab_size], initializer=tf.contrib.layers.xavier_initializer())
+    output_weights = tf.get_variable("output_weights", shape=[hidden_size, vocab_size], initializer=tf.contrib.layers.xavier_initializer())
+    output_bias = tf.get_variable("output_bias", shape=[vocab_size], initializer=tf.contrib.layers.xavier_initializer())
 
-    if down_project:
+    if down_project: #experiment C
         down_project_weights = tf.get_variable("down_project_weights", shape=[hidden_size*2, hidden_size], initializer=tf.contrib.layers.xavier_initializer())
         down_project_bias = tf.get_variable("down_project_bias", shape=[hidden_size], initializer=tf.contrib.layers.xavier_initializer())
 
     #fancy way of interating over time_steps which is middle dimension of embedded_inputs (batch_size x time_steps x embedding_size)
     output_list = []
-    prediction_list = [] #not required for training
+    prediction_list = []
     embedded_inputs_unstacked = tf.unstack(embedded_inputs, axis=1)
+
     for embedded_input in embedded_inputs_unstacked:
         #calculate next state of rnn cell
         _, state = rnn(embedded_input, state)
 
         #calculate output layer
-        if down_project:
+        if down_project: #experiment C
             down_projected = tf.matmul(state.h, down_project_weights) + down_project_bias
             output = tf.matmul(down_projected, output_weights) + output_bias
-        else:
+        else: #experiment A,B
             output = tf.matmul(state.h, output_weights) + output_bias
-        #TODO "de-embed"???
-        if deembed:
-            output = tf.matmul(output, tf.transpose(embedding_weights))
         output_list += [output]
-        prediction_list += [tf.argmax(output,axis=1)] #not required for training
-    outputs = tf.stack(output_list, axis=1) # (batch_size x time_steps x embedding_size
-    predictions = tf.stack(prediction_list, axis=1) #not required for training
+
+        #store prediction
+        prediction_list += [tf.argmax(output,axis=1)] #TODO take softmax before argmax???
+
+    outputs = tf.stack(output_list, axis=1) # (batch_size x time_steps x embedding_size)
+    predictions = tf.stack(prediction_list, axis=1) # (batch size x time_steps)
 
 with tf.variable_scope("optimizer"):
     #calculate loss
-    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=outputs[:,:-1,:], labels=targets[:,1:])) #TODO take mean???
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=outputs[:,:-1,:], labels=targets[:,1:])) #TODO take sum instead of mean???
     #define optimizer
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
     #clip gradients
@@ -100,15 +94,13 @@ with tf.variable_scope("optimizer"):
     train_op = optimizer.apply_gradients(zip(grad,var))
     #logging
     tf.summary.scalar("loss", loss)
-    merged = tf.summary.merge_all()
+    log_op = tf.summary.merge_all()
 
 #initialise graph
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-global_step = 0
 
-#load word2vec
-if use_word2vec:
+if use_word2vec: #experiment B: load pretrained word2vec embedding weights
     load_embedding(sess, vocab, embedding_weights, "./wordembeddings-dim100.word2vec", embedding_size, vocab_size)
 
 #logging
@@ -120,38 +112,32 @@ model_dir = os.path.abspath(os.path.join(log_dir, "checkpoints"))
 model_prefix = os.path.join(model_dir, "model")
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
-saver = tf.train.Saver(tf.global_variables()) #, max_to_keep=number_checkpoint)
+saver = tf.train.Saver(tf.global_variables(), max_to_keep=number_checkpoint) #TODO does max to keep refer to first or last checkpoints?
 
 #perform training step and return loss
 def train(x, y, step):
-    l,m,_ = sess.run([loss, merged, train_op], feed_dict={inputs: x, targets: y}) #train
-    writer.add_summary(m, step) #add (step,loss) to summary for visualising loss 
+    l,log,_ = sess.run([loss, log_op, train_op], feed_dict={inputs: x, targets: y}) #train
+    writer.add_summary(log, step) #add (step,loss) to summary for visualising loss 
     return l
 
-def predict(x): #not required for training
+def predict(x): #TODO not confirmed to work yet
     return sess.run(predictions, feed_dict={inputs: x})
 
 #DRIVER
-
 batcher = Batcher(data)
 
 #train
-for i in range(n_train_steps):
-    global_step += 1
+for step in range(n_train_steps):
     x = y = batcher.get_batch(batch_size)
-    l = train(x, y, i)
-    if i % 1 == 0:
-        print("{:.1f}% loss={}".format(100*i/n_train_steps, l))
-    if (i+1) % checkpoint_time == 0:
-        model_path = saver.save(sess, model_prefix, global_step=global_step)
+    l = train(x, y, step)
+    if step % 1 == 0:
+        print("{:.1f}% loss={}".format(100*step/n_train_steps, l))
+    if (step+1) % checkpoint_time == 0:
+        model_path = saver.save(sess, model_prefix, global_step=step)
         print("Model saved to {}\n".format(model_path))
 
-model_path = saver.save(sess, model_prefix, global_step=global_step)
+model_path = saver.save(sess, model_prefix, global_step=n_train_steps-1)
 print("Final model saved to {}\n".format(model_path))
-#test
-#predicted = predict(np.reshape([data]*batch_size, [batch_size, input_n]))
-#for i in range(input_n-1): 
-#    print(predicted[:,i], data[i+1])
 
 #logging
 writer.close()
